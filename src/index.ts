bccfe79f7528959408359656c8c41a7ede801abc
Gihyeon Sung <gihyeon@gihyeon.xyz>
2022-09-29 19:02:22 +0900

    feat: replace any type

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
-export const handler = async (event: any, context: any): Promise<any> => {
+import { Context, APIGatewayProxyResult, APIGatewayEvent } from 'aws-lambda';
+
+export const handler = async (event: APIGatewayEvent, context: Context): Promise<APIGatewayProxyResult> => {
   console.log(`Event: ${JSON.stringify(event, null, 2)}`);
   console.log(`Context: ${JSON.stringify(context, null, 2)}`);
   return {
