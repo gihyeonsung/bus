58c80056ca8128a02b4f073fbd903efca3a34f88
Gihyeon Sung <gihyeon@gihyeon.xyz>
2022-10-14 19:05:12 +0900

    tmp

--- src/repo/channelRepo.ts
+++ src/repo/channelRepo.ts
@@ -1,5 +1,25 @@
 import { Channel } from '../entity'
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import { DynamoDBDocumentClient } from '@aws-sdk/lib-dynamodb';
 
 export interface IChannelRepo {
   GetAll(): Promise<Channel>
+}
+
+export class DynamoDBChannelRepo implements IChannelRepo {
+  private client: DynamoDBDocumentClient;
+
+  constructor(private ddbConfig: any) {
+    this.client = DynamoDBDocumentClient.from(new DynamoDBClient(ddbConfig));
+  }
+
+  async GetAll(): Promise<Channel> {
+    this.client.send();
+
+    throw new Error('Method not implemented.')
+  }
+
+  async destory() {
+    this.client.destroy();
+  }
 }
